faddab120e4cbf26d821facf38046d628df4986b
Luis Zabala <luis.zabala076@gmail.com>
2019-02-25 01:41:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,45 +1,49 @@
 class Application
 
- @@cart =["Apples","Carrotsmell","Pears"]
-  @@items = ["Apples","Carrots","Pears"]	  @@items = ["Apples","Carrots","Pears"]
+  @@items = ["Apples","Carrots","Pears"]
+  @@cart  = []
+  
+  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
-
-   def call(env)	  def call(env)
-@@ -10,16 +10,35 @@ def call(env)
-      @@items.each do |item|	      @@items.each do |item|
-        resp.write "#{item}\n"	        resp.write "#{item}\n"
-      end	      end
-    elsif req.path.match(/cart/)
-      if @@cart.empty?
-        resp.write"Your cart is empty"
-        else
-            @@cart.each do |item|
-            resp.write "#{item}\n"
-        end
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
       end
-    elsif req.path.match(/search/)	    elsif req.path.match(/search/)
-      search_term = req.params["q"]	      search_term = req.params["q"]
-      resp.write handle_search(search_term)	      resp.write handle_search(search_term)
-      elsif req.path.match(/add/)
-        added= req.params["item"]
-    if @@items.include? added
-        @@cart  <<  added
-        resp.write  "added #{added}"
+      elsif req.path.match(/cart/)
+      if @@cart.empty?
+      req.write "Your cart is empty"
+    else 
+      @@cart.each do |c|
+        req.write "#{c}\n"
+    end
+  end 
+
+    def req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    elsif req.path.match(/add/)
+        c_cart = req.params["item"]
+    if @@items.include? c_cart
+        @@cart  <<  c_cart
+        resp.write  "You added #{c_cart}"
       else
         resp.write "We don't have that item"
       end
-    else	    else
-      resp.write "Path Not Found"	      resp.write "Path Not Found"
-    end	    end
+      resp.write "Path Not Found"
+    end
 
+    resp.finish
+  end
+      
 
-     resp.finish	    resp.finish
-  end	  end
-
-
-
-
-
-   def handle_search(search_term)	  def handle_search(search_term)
-    if @@items.include?(search_term)	    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"	      return "#{search_term} is one of our items"
+  def handle_search(search_term)
+    if @@items.include?(search_term)
+      return "#{search_term} is one of our items"
+    else
+      return "Couldn't find #{search_term}"
+    end
+  end
+end
